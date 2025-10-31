4877274e589382775171cf76a88b0b17ce08da5d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-30 20:38:59 -0400

    Create get-dependencies.sh

---
+++ get-dependencies.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+set -eu
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+echo "Installing build dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	base-devel             \
+	cmake                  \
+	curl                   \
+	git                    \
+	libpulse               \
+	libx11                 \
+	libxrandr              \
+	libxss                 \
+	pulseaudio             \
+	pulseaudio-alsa        \
+	pipewire-audio         \
+	wget                   \
+	xorg-server-xvfb       \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-opengl libxml2-mini opus-mini gdk-pixbuf2-mini
+
+echo "Building android-translation-layer..."
+echo "---------------------------------------------------------------"
+sed -i -e 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+sed -i \
+	-e 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|'  \
+	-e 's|#MAKEFLAGS|MAKEFLAGS|'                 \
+	/etc/makepkg.conf
+cat /etc/makepkg.conf
+
+git clone --depth 1 https://aur.archlinux.org/android_translation_layer-git.git ./android-layer
+(
+	cd ./android-layer
+	makepkg -fs --noconfirm
+	ls -la .
+	pacman --noconfirm -U ./*.pkg.tar.*
+)
+
+pacman -Q android_translation_layer-git | awk '{print $2; exit}' > ~/version
