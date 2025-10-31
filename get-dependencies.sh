cd3a07d1fb60ab38f9d5dd9a263d7b10b524d574
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-31 00:01:56 -0400

    try chaotic AUR

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -38,14 +38,26 @@ sed -i \
 	/etc/makepkg.conf
 cat /etc/makepkg.conf
 
-git clone --depth 1 https://aur.archlinux.org/yay-bin.git ./yay
-(
-	cd ./yay
-	makepkg -fs --noconfirm
-	ls -la .
-	pacman --noconfirm -U ./*.pkg.tar.*
-)
+# does not build in CI 
+#System has not been booted with systemd as init system (PID 1). Can't operate
+#
+#git clone --depth 1 https://aur.archlinux.org/yay-bin.git ./yay
+#(
+#	cd ./yay
+#	makepkg -fs --noconfirm
+#	ls -la .
+#	pacman --noconfirm -U ./*.pkg.tar.*
+#)
 	
-yay -S --noconfirm android_translation_layer-git
+#yay -S --noconfirm android_translation_layer-git
+
+pacman-key --init
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo '[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
+pacman -Syu --noconfirm android_translation_layer-git
 
 pacman -Q android_translation_layer-git | awk '{print $2; exit}' > ~/version
