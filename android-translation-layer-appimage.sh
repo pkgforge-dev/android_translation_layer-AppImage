530dc1fdcb94379e0e95c64429b1803478f76c4a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-30 20:26:25 -0400

    Add files via upload

---
+++ android-translation-layer-appimage.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -eu
+
+ARCH="$(uname -m)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+VERSION="$(cat ~/version)"
+
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export DEPLOY_OPENGL=1
+export DEPLOY_GSTREAMER=1
+export ICON=DUMMY
+export OUTNAME=Android_Translation_Layer-"$VERSION"-anylinux-"$ARCH".AppImage
+
+# ADD LIBRARIES
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/android-translation-layer
+
+cp -rv /usr/lib/java  ./AppDir/lib
+cp -rv /usr/share/atl ./AppDir/share
+
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
+
+mkdir -p ./dist
+mv -v ./*.AppImage* ./dist
+mv -v ~/version     ./dist
