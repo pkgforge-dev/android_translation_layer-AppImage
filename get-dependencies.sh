a95a1ae6f11e360aba6887f53e73e7026d5723da
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-01 04:13:00 -0400

    try to fix broken mess

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm pipewire-audio
+pacman -Syu --noconfirm pipewire-audio patchelf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -15,4 +15,8 @@ get-debloated-pkgs --add-mesa libxml2-mini opus-mini gdk-pixbuf2-mini librsvg-mi
 # Comment this out if you need an AUR package
 make-aur-package --chaotic-aur android_translation_layer-git
 
-# If the application needs to be manually built that has to be done down here
+# the art_standalone package is broken since it links to a no longer existing version of icu
+# try to fix it with a hack
+l=/usr/lib/java/dex/art/natives/libjavacore.so
+patchelf --replace-needed libicuuc.so.76 libicui18n.so "$l"
+patchelf --replace-needed libicui18n.so.76 libicui18n.so "$l"
