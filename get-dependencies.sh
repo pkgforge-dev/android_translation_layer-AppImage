a21f96238b92c23f974cb2fc2b8395174f0350d4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-01 05:32:27 -0400

    build hack

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,3 +19,10 @@ make-aur-package --chaotic-aur android_translation_layer-git
 wget 'https://archive.archlinux.org/packages/i/icu/icu-76.1-1-x86_64.pkg.tar.zst' -O ./icu.pkg.tar.zst
 tar xvf ./icu.pkg.tar.zst
 cp -rn ./usr/lib/* /usr/lib
+
+# we need to build a library to preload to fix an issue with the generated desktop 
+# entries having the full path to the tmp binary mountpoint instead of the appimage
+cc -shared -fPIC -O2 -Wall -Wextra -o libfixargv0.so fixargv0.c -ldl
+mkdir -p ./AppDir/shared/lib
+cp -v ./libfixargv0.so ./AppDir/shared/lib
+echo 'libfixargv0.so' >> ./AppDir/.preload
