031b2992e26b9ef1e26edb7d420cdc352fb03c74
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-01 20:32:25 -0400

    chaotic-aur fixed the package

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,8 +14,3 @@ get-debloated-pkgs --add-mesa libxml2-mini opus-mini gdk-pixbuf2-mini librsvg-mi
 
 # Comment this out if you need an AUR package
 make-aur-package --chaotic-aur android_translation_layer-git
-
-# the art_standalone package is broken since it links to a no longer existing version of icu
-wget 'https://archive.archlinux.org/packages/i/icu/icu-76.1-1-x86_64.pkg.tar.zst' -O ./icu.pkg.tar.zst
-tar xvf ./icu.pkg.tar.zst
-cp -rn ./usr/lib/* /usr/lib
